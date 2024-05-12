26b23f6afefe6ff6732738c043e1a7717ae9d390
Meseverri <meseverrimarquez5@gmail.com>
2024-05-12 11:01:56 +0200

    server

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const { connectDB } = require("./src/config/DB");
 const express = require("express");
-
+const {puzzlesRouter}=require("./src/api/rutas/Puzzle")
 
 connectDB();
 const PORT=3000;
-const server = express();
+const app = express();
 
 
 app.use(express.json()); 
+
+app.use("/api/v1/puzzles",puzzlesRouter);
 
 app.use("*",(req,res,next)=>{
     return res.status(404).json("Route not found");
--- src/api/modelos/Plant.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-
-
-const plantSchema= new mongoose.Schema(
-    {
-        
-    }
-)
