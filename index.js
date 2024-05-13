8ff4e2a4306cf6008622efcfc72295f31fff6be6
Meseverri <meseverrimarquez5@gmail.com>
2024-05-13 10:18:59 +0200

    app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { connectDB } = require("./src/config/DB");
 const express = require("express");
 const {puzzlesRouter}=require("./src/api/rutas/Puzzle")
+const {clientRouter}=require("./src/api/rutas/Client")
 
 connectDB();
 const PORT=3000;
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json()); 
 
 app.use("/api/v1/puzzles",puzzlesRouter);
+app.use("/api/v1/clients",clientRouter);
 
 app.use("*",(req,res,next)=>{
     return res.status(404).json("Route not found");
