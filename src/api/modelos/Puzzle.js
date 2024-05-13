d814e5b211cf6ef9785b02528ebfb6fea50e596f
Meseverri <meseverrimarquez5@gmail.com>
2024-05-13 07:51:52 +0200

    PuzzleSchema reformated

--- src/api/modelos/Puzzle.js
+++ src/api/modelos/Puzzle.js
@@ -1,16 +1,18 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-
-const puzzleSchema= new mongoose.Schema(
-    {
-        brand:{type:String ,required:true},
-        price:{type:Number,required:true},
-        subcategory:{type:String,required:true},
-        name:{type: String,required:true},
-        stock:{type:String,required:true}
-    }
+const puzzleSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    img: { type: String },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Puzzle= mongoose.model('Puzzle', puzzleSchema,"puzzles");
+const Puzzle = mongoose.model("Puzzle", puzzleSchema, "puzzles");
 
 module.exports = Puzzle;
---
+++ src/utils/Data/data.js
@@ -0,0 +1,112 @@
+const products = [
+    {
+      name: "QiYi Warior 3x3 S",
+      price: 3.9,
+      type: "3x3",
+      brand: "QiYi",
+      image: ".//qiyi-warrior-3x3-s.webp",
+    },
+    {
+      name: "Shengshou Legend 3x3",
+      price: 6.35,
+      type: "3x3",
+      brand: "Shengshou",
+      image: ".//shengshou-legend-3x3.webp",
+    },
+    {
+      name: "QiYi Mirror 3x3",
+      price: 5.25,
+      type: "Modificaciones 3x3",
+      brand: "QiYi",
+      image: "/qiyi-mirror-3x3.webp",
+    },
+    {
+      name: "QiYi Axis 3x3",
+      price: 5.25,
+      type: "Modificaciones 3x3",
+      brand: "QiYi",
+      image: ".//qiyi-axis-3x3.webp",
+    },
+    {
+      name: "YJ YuPo 2x2 V2M ",
+      price: 6.9,
+      type: "2x2",
+      brand: "YJ",
+      image: "/yj-yupo-2x2-v2-m.webp",
+    },
+    {
+      name: "Lee 2x2 windmill cube",
+      price: 6.35,
+      type: "Modificaciones 2x2",
+      brand: "Lee",
+      image: "/lee-2x2-windmill-cube.webp",
+    },
+    {
+      name: "DianSheng 8 Corner",
+      price: 12.90,
+      type: "Modificaciones 4x4",
+      brand: "DianSheng",
+      image: "/diansheng-8-corner.webp",
+    },
+    {
+      name: "MeiLong 4x4",
+      price: 5.25,
+      type: "4x4",
+      brand: "MeiLong",
+      image: "/meilong-4x4.webp",
+    },
+    {
+      name: "SengSo Crazy 5x5 V2",
+      price: 22.99,
+      type: "5x5",
+      brand: "Shengshou",
+      image: "/sengso-crazy-5x5-v2.webp",
+    },
+    {
+      name: "QiYi QiFang S2 6x6",
+      price: 14.9,
+      type: "6x6",
+      brand: "QiYi",
+      image: "/qiyi-qifang-s2-6x6.webp",
+    },
+    {
+      name: "MoYu Aofu 7x7 WRM",
+      price: 49.9,
+      type: "7x7",
+      brand: "MoYu Cube",
+      image: "/moyu-aofu-7x7-wrm.webp",
+    },
+    {
+      name: "MeiLong 12x12",
+      price: 124.95,
+      type: "12x12",
+      brand: "MeiLong",
+      image: "/meilong-12x12.webp",
+    },
+
+    {
+      name: "QiYi QiHeng Megaminx",
+      price: 7.95,
+      type: "Megaminx",
+      brand: "Qiyi",
+      image: "/qiyi-qiheng-megaminx.webp",
+    },
+
+    {
+      name: "ShengShou Elite Kilominx",
+      price: 67.90,
+      type: "Kilominx",
+      brand: "ShengShou",
+      image: "/shengshou-elite-kilominx.webp",
+    },
+
+    {
+      name: "YJ YuHu Megaminx V2 M",
+      price: 14.99,
+      type: "Megaminx",
+      brand: "YJ",
+      image: "/yj-yuhu-megaminx-v2-m.webp",
+    },
+
+  ];
+export default products
