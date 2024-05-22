4f9a2dc55c88304834a9cb93cb1df24bbd2d02b4
Meseverri <meseverrimarquez5@gmail.com>
2024-05-22 10:54:26 +0200

    $ push bug fixed

--- src/api/contoladores/Client.js
+++ src/api/contoladores/Client.js
@@ -1,41 +1,69 @@
 const Client = require("../modelos/Client");
 
-
-const getUser= async (req,res,next)=>{
-    try {
-        const {id}=req.params;
-        const user = await Client.findById(id).populate("cart");
-        return res.status(200).json(user)
-    } catch (error) {
-        return res.status(400).json("error-> "+error);
-    } 
-}
-
-const createUser= async (req,res)=>{
-    try {
-        const newClient=new Client(req.body)
-        const clientSaved= await newClient.save()
-        return res.status(201).json(clientSaved);
-    } catch (error) {
-        return res.status(400).json("error-> "+error);
-        
-    }
-}
-
-const modifyUser=async (req,res)=>{
-    try {
+const getUser = async (req, res, next) => {
+  try {
     const { id } = req.params;
-    const newClient= new Client(req.body);
-    newClient._id=id;
-    const updatedClient= await Client.findByIdAndUpdate(id,newClient,{new:true});
-    return res.status(200).json(updatedClient)
-    } catch (error) {
-        return res.status(400).json("error "+error);
-    }
-}
+    const user = await Client.findById(id).populate("cart");
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json("error-> " + error);
+  }
+};
 
-module.exports={
-    getUser,
-    createUser,
-    modifyUser
-}
+const createUser = async (req, res) => {
+  try {
+    const newClient = new Client(req.body);
+    const clientSaved = await newClient.save();
+    return res.status(201).json(clientSaved);
+  } catch (error) {
+    return res.status(400).json("error-> " + error);
+  }
+};
+
+const modifyUser = async (req, res) => {
+  try {
+    //agarramos el id de los parametros
+    const { id } = req.params;
+    const newToCart = req.body.cart;
+    const modifiedName = req.body.name;
+
+    let updatedClient;
+    //Aqui valida
+    if (modifiedName && newToCart) {
+      updatedClient = await Client.findByIdAndUpdate(
+        id,
+        {
+          name: modifiedName,
+          $push: { cart: newToCart },
+        },
+        { new: true }
+      );
+    } else if (newToCart) {
+      updatedClient = await Client.findByIdAndUpdate(
+        id,
+        {
+          $push: { cart: newToCart },
+        },
+        { new: true }
+      );
+    } else if (modifiedName) {
+      updatedClient = await Client.findByIdAndUpdate(
+        id,
+        {
+          name: modifiedName,
+        },
+        { new: true }
+      );
+    }
+
+    return res.status(200).json(updatedClient);
+  } catch (error) {
+    return res.status(400).json("error " + error);
+  }
+};
+
+module.exports = {
+  getUser,
+  createUser,
+  modifyUser,
+};
--- src/utils/seeds/seed.js
+++ src/utils/seeds/seed.js
@@ -6,18 +6,15 @@ require("dotenv").config();
 mongoose
   .connect(process.env.DB_URL)
   .then(async () => {
-		// Buscamos todas las películas de nuestra colección
+		// Buscamos todas las puzzles de nuestra colección
     const allPuzzle = await Puzzle.find();
 
-
-    if (allPuzzle.length) {
-      await Puzzle.collection.drop(); //La función drop borra la colección
-    }
+    await Puzzle.collection.drop(); //La función drop borra la colección
   })
   .catch((err) => console.log(`Error deleting data: ${err}`))
   .then(async () => {
-		// Una vez vaciada la colección de las películas, usaremos el array Puzzle de nuestra carpeta data
-		// para llenar nuestra base de datos con todas las películas.
+		// Una vez vaciada la colección de las puzzles, usaremos el array Puzzle de nuestra carpeta data
+		// para llenar nuestra base de datos con todas las puzzles.
 		await Puzzle.insertMany(products);
 	})
   .catch((err) => console.log(`Error creating data: ${err}`))
